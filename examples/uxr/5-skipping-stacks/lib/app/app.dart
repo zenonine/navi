adf5e272e9fb4454b20ba8d9271373da45d0e520
Xuan Dai Nguyen <nguyenxndaidev@gmail.com>
2021-03-29 10:12:10 +0200

    Fix bug: https://github.com/zenonine/navi/issues/14

--- examples/uxr/5-skipping-stacks/lib/app/app.dart
+++ examples/uxr/5-skipping-stacks/lib/app/app.dart
@@ -3,29 +3,26 @@ import 'package:navi/navi.dart';
 
 import 'index.dart';
 
-class App extends StatefulWidget {
-  @override
-  _AppState createState() => _AppState();
-}
+class App extends StatelessWidget {
+  final _informationParser = NaviInformationParser();
+  final _routerDelegate = NaviRouterDelegate(
+    rootStack: InlinePageStack(
+      pages: (BuildContext context) => [
+        MaterialPage<dynamic>(
+          key: const ValueKey('Root'),
+          child: RootPage(),
+        ),
+      ],
+    ),
+  );
 
-class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Navi - Declarative navigation API for Flutter',
       debugShowCheckedModeBanner: false,
-      routeInformationParser: NaviInformationParser(),
-      routerDelegate: NaviRouterDelegate(
-        rootStack: InlinePageStack(
-          pages: (BuildContext context) =>
-          [
-            MaterialPage<dynamic>(
-              key: const ValueKey('Root'),
-              child: RootPage(),
-            ),
-          ],
-        ),
-      ),
+      routeInformationParser: _informationParser,
+      routerDelegate: _routerDelegate,
     );
   }
 }
