dc6126529d6990153d45e6cecc7c10b4862c83fa
Xuan Dai Nguyen <nguyenxndaidev@gmail.com>
2021-04-05 17:07:13 +0200

    Implement skeleton for scenario 4a-nested-routing-with-tabs

--- examples/uxr/4a-nested-routing-with-tabs/lib/app/root_page.dart
+++ examples/uxr/4a-nested-routing-with-tabs/lib/app/root_page.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
+enum AppTab { books, settings }
+
 class RootPage extends StatefulWidget {
   @override
   _RootPageState createState() => _RootPageState();
 }
 
 class _RootPageState extends State<RootPage> {
+  var _currentTab = AppTab.books;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Root Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bookstore'),
+      ),
+      body: Text('$_currentTab'),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentTab.index,
+        onTap: (tabIndex) {
+          setState(() {
+            _currentTab = tabIndex == 0 ? AppTab.books : AppTab.settings;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Books',
+            icon: Icon(Icons.chrome_reader_mode),
+          ),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
     );
   }
 }
