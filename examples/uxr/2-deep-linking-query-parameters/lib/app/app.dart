035d117ef0649a79ea73f69719470b70d48882db
Xuan Dai Nguyen <nguyenxndaidev@gmail.com>
2021-05-01 18:11:06 +0200

    App should dispose router delegate

--- examples/uxr/2-deep-linking-query-parameters/lib/app/app.dart
+++ examples/uxr/2-deep-linking-query-parameters/lib/app/app.dart
@@ -3,15 +3,25 @@ import 'package:navi/navi.dart';
 
 import 'index.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   final _informationParser = NaviInformationParser();
   final _routerDelegate = NaviRouterDelegate.material(child: BooksStack());
+
+  @override
+  void dispose() {
+    _routerDelegate.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Navi - Declarative navigation API for Flutter',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textSelectionTheme: const TextSelectionThemeData(
           selectionColor: Colors.white70,
